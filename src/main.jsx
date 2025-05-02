615c9dacaa836f387608d371c462bd5934b26772
Lucia Aguirre <guerrolucia@gmail.com>
2025-05-02 18:05:43 +0200

    fix: installs correct version of chakra ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import { Provider } from './components/ui/provider'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider theme={theme}>
       <App />
-    </Provider>
+    </ChakraProvider>
   </StrictMode>
 )
