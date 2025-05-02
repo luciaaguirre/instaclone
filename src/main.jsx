f34fbf3d2ca3303bc4d0cfdb62f72c6a6dde3691
Lucia Aguirre <guerrolucia@gmail.com>
2025-05-02 18:27:05 +0200

    adds and configures dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
 
-const theme = extendTheme({
-  config: {
-    initialColorMode: 'dark',
-    useSystemColorMode: false,
-  },
-})
+//selecing the exacto colors whe want in light mode and dark mode (just gonna use dark mode)
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode('gray.100', '#000')(props),
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+    },
+  }),
+}
+
+//color mode config
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+//theme extension
+const theme = extendTheme({ config, styles })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
